a36eda0e9b76e17c97864210296c83cb5a7a10a8
ShecktorS <ettoresanfilippo@gmail.com>
2023-06-28 12:19:12 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <h1>Login</h1>
+      <h1>Board</h1>
+    </div>
+  );
+}
+
+export default App;
